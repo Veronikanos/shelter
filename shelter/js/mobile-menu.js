4522654190da5eaddc5d8885cdad96571b60f5f9
Veronika Tlostiuk <vveronikanika@gmail.com>
2023-04-09 18:15:54 +0300

    feat: style burger-icon and rotate

---
+++ shelter/js/mobile-menu.js
@@ -0,0 +1,39 @@
+const hamburger = document.querySelector('.nav-button');
+const popup = document.querySelector('#popup');
+const body = document.body;
+
+const menu = document.querySelector('#menu').cloneNode(1);
+
+hamburger.addEventListener('click', hamburgerHandler);
+
+function hamburgerHandler(e) {
+  e.preventDefault();
+  popup.classList.toggle('open');
+  // popup.classList.toggle('visually-hidden');
+  hamburger.classList.toggle('active');
+  body.classList.toggle('noscroll');
+  renderPopup();
+}
+
+function renderPopup() {
+  popup.appendChild(menu);
+}
+const links = Array.from(menu.children);
+
+links.forEach((link) => {
+  link.addEventListener('click', closeOnClick);
+});
+
+function closeOnClick() {
+  popup.classList.remove('open');
+  hamburger.classList.remove('active');
+  body.classList.remove('noscroll');
+}
+
+document.addEventListener('click', function (event) {
+  if (!popup.classList.contains('open') || event.target === hamburger)
+    return;
+
+  const isClickInsideMenu = menu.contains(event.target);
+  if (!isClickInsideMenu) closeOnClick();
+});
