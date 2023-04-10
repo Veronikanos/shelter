6348a043f31540b534c8f7dfdc8df63b3ec1f9a4
Veronika Tlostiuk <vveronikanika@gmail.com>
2023-04-10 17:04:38 +0300

    fix: bug with generation cards on desktop, change container width

--- shelter/js/slider.js
+++ shelter/js/slider.js
@@ -8,6 +8,8 @@ const petsCards = document.querySelector('.our-friends-list');
 const nextButton = document.querySelector('button.next');
 const prevButton = document.querySelector('button.prev');
 
+start();
+
 async function start() {
   try {
     const res = await fetch('./assets/data.json');
@@ -19,7 +21,20 @@ async function start() {
   init();
 }
 
-start();
+function init() {
+  const sliderWidth = getSlidersWidth();
+  generateArr(nextArr);
+  currArr.push(...nextArr);
+  nextArr = [];
+  generateArr(nextArr);
+  pastArr.push(...currArr);
+  currArr = [];
+  currArr.push(...nextArr);
+  nextArr = [];
+  generateArr(nextArr);
+  petsCards.innerHTML = generateCards().join('');
+  console.log([...pastArr, ...currArr, ...nextArr]);
+}
 
 nextButton.addEventListener('click', moveRight);
 prevButton.addEventListener('click', moveLeft);
@@ -32,39 +47,10 @@ petsCards.addEventListener('animationend', (e) => {
     petsCards.classList.remove('transition-left');
     backward();
   }
+  petsCards.innerHTML = generateCards().join('');
   nextButton.addEventListener('click', moveRight);
   prevButton.addEventListener('click', moveLeft);
 });
-
-function init() {
-  const sliderWidth = getSlidersWidth();
-
-  // - 1. генерируем массив nextArr;
-  generateArr(nextArr);
-
-  // - 2. перемещаем значения из массива nextArr (попутно его обнуляя) в массив currArr;
-  currArr.push(...nextArr);
-  nextArr = [];
-
-  // - 3. генерируем массив nextArr (помним про проверку на наличие значений в currArr);
-  generateArr(nextArr);
-
-  // - 4. перемещаем значения из массива currArr (попутно его обнуляя) в массив pastArr;
-  pastArr.push(...currArr);
-  currArr = [];
-
-  // - 5. перемещаем значения из массива nextArr (попутно его обнуляя) в массив currArr;
-  currArr.push(...nextArr);
-  nextArr = [];
-
-  // - 6. генерируем массив nextArr (помним про проверку на наличие значений в currArr);
-  generateArr(nextArr);
-
-  // console.log(pastArr, currArr, nextArr);
-
-  petsCards.innerHTML = generateCards().join('');
-  // return > (pastArr([1,2,3]), currArr([4,5,6]), nextArr([7,8,1]))
-}
 
 function generateCards() {
   let markup = [];
@@ -100,21 +86,13 @@ function moveLeft() {
 }
 
 function forward() {
-  // 	- 1. обнуляем массив pastArr;
   pastArr = [];
-
-  // - 2. перемещаем значения из массива currArr (попутно его обнуляя) в массив pastArr;
   pastArr.push(...currArr);
   currArr = [];
-
-  // - 3. перемещаем значения из массива nextArr (попутно его обнуляя) в массив currArr;
   currArr.push(...nextArr);
   nextArr = [];
-
-  // - 4. генерируем массив nextArr (помним про проверку на наличие значений в currArr).
   generateArr(nextArr);
-  petsCards.innerHTML = generateCards().join('');
-  // nextButton.addEventListener('click', forward);
+  // petsCards.innerHTML = generateCards().join('');
 }
 
 function backward() {
@@ -124,31 +102,31 @@ function backward() {
   currArr.push(...pastArr);
   pastArr = [];
   generateArr(pastArr);
-  petsCards.innerHTML = generateCards().join('');
+  // petsCards.innerHTML = generateCards().join('');
 }
 
-// function changeToBackward() {
-//   // - 1. меняем местами значения в массивах pastArr и currArr;
-//   const temp = [];
-//   temp.push(...pastArr);
-//   pastArr.push(...currArr);
-//   currArr.push(...temp);
+// // function changeToBackward() {
+// //   // - 1. меняем местами значения в массивах pastArr и currArr;
+// //   const temp = [];
+// //   temp.push(...pastArr);
+// //   pastArr.push(...currArr);
+// //   currArr.push(...temp);
 
-//   // - 2. обнуляем значеничия массива nextArr;
-//   nextArr = [];
-//   // - 3. генерируем массив nextArr (помним про проверку на наличие значений в currArr).
-//   generateArr(nextArr);
-// }
+// //   // - 2. обнуляем значеничия массива nextArr;
+// //   nextArr = [];
+// //   // - 3. генерируем массив nextArr (помним про проверку на наличие значений в currArr).
+// //   generateArr(nextArr);
+// // }
 
-// function changeToForward() {
-//   const temp = [];
-//   temp.push(...nextArr);
-//   nextArr.push(...currArr);
-//   currArr.push(...temp);
+// // function changeToForward() {
+// //   const temp = [];
+// //   temp.push(...nextArr);
+// //   nextArr.push(...currArr);
+// //   currArr.push(...temp);
 
-//   pastArr = [];
-//   generateArr(pastArr);
-// }
+// //   pastArr = [];
+// //   generateArr(pastArr);
+// // }
 
 function generateArr(arr) {
   for (let i = 0; i < numberOfCards; i++) {
@@ -156,9 +134,8 @@ function generateArr(arr) {
     let elementFromData = allPets[randomNumber];
 
     if (
-      pastArr.includes(elementFromData) ||
-      currArr.includes(elementFromData) ||
-      nextArr.includes(elementFromData)
+      arr.includes(elementFromData) ||
+      currArr.includes(elementFromData)
     ) {
       i--;
     } else {
@@ -168,14 +145,13 @@ function generateArr(arr) {
 }
 
 function getSlidersWidth() {
-  const myDiv = document.querySelector('.our-friends__slider');
-  const width = myDiv.offsetWidth;
+  const width = document.body.offsetWidth;
   console.log('Width: ' + width);
 
   numberOfCards = 2;
-  if (width < 580) {
+  if (width < 768) {
     numberOfCards = 1;
-  } else if (width > 580) {
+  } else if (width >= 1280) {
     numberOfCards = 3;
   }
 
